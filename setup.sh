f8321947a263ca84c01cc81ad060a637332b9c32
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-10-30 15:32:29 -0300

    Set minters in setup script.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,27 @@
 #!/bin/zsh
 
+NETWORK_1=arbitrum
+$NETWORK_1U=Arbitrum
+NETWORK_2=polygon
+NETWORK_2U=Polygon
+
 echo "Deploying Launchpad token on Arbitrum chain."
 
-read TOKEN_1 < <(yarn deploy --tags Launchpad --network arbitrum | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
+read TOKEN_1 < <(yarn deploy --tags Launchpad --network $NETWORK_1 | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
 
 echo "Arbitrum Launchpad token deployed to $TOKEN_1. Deploying Launchpad token on Optimism chain."
 
-read TOKEN_2 < <(yarn deploy --tags Launchpad --network polygon | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
+read TOKEN_2 < <(yarn deploy --tags Launchpad --network $NETWORK_2 | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
 
 echo "Setting up Wormhole NTT for Launchpad tokens ($TOKEN_1 / $TOKEN_2)."
 
 cd wh-token &&
 rm deployment.json ||
 ntt init Mainnet
-ntt add-chain Arbitrum --latest --mode burning --token $TOKEN_1 --skip-verify
-ntt add-chain Polygon --latest --mode burning --token $TOKEN_2 --skip-verify
+ntt add-chain $NETWORK_1U --latest --mode burning --token $TOKEN_1 --skip-verify
+ntt add-chain $NETWORK_2U --latest --mode burning --token $TOKEN_2 --skip-verify
 cp deployment.json ../packages/nextjs/contracts/deployment.json
 cp deployment.json ../packages/hardhat/deploy/deployment.json ||
 cd ..
+yarn deploy --tags SetMinter --network $NETWORK_1
+yarn deploy --tags SetMinter --network $NETWORK_2
