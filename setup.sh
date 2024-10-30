9626bad11986d1b1748dea908bd1eb3728465212
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-10-30 15:53:16 -0300

    Fix setup script.

--- setup.sh
+++ setup.sh
@@ -5,23 +5,27 @@ NETWORK_1U=Arbitrum
 NETWORK_2=polygon
 NETWORK_2U=Polygon
 
-echo "Deploying Launchpad token on Arbitrum chain."
-
-read TOKEN_1 < <(yarn deploy --tags Launchpad --network $NETWORK_1 | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
-
-echo "Arbitrum Launchpad token deployed to $TOKEN_1. Deploying Launchpad token on Optimism chain."
+echo "Deploying Launchpad token on $NETWORK_2U chain."
 
 read TOKEN_2 < <(yarn deploy --tags Launchpad --network $NETWORK_2 | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
 
+echo $TOKEN_2
+
+echo "Deploying Launchpad token on $NETWORK_1U chain."
+
+read TOKEN_1 < <(yarn deploy --tags Launchpad --network $NETWORK_1 | grep -E 'deploying "LaunchpadToken"|reusing "LaunchpadToken"' | sed -E 's/.*(deployed at|at) ([^ ]+).*/\2/' | head -n 1)
+
+echo $TOKEN_1
+
 echo "Setting up Wormhole NTT for Launchpad tokens ($TOKEN_1 / $TOKEN_2)."
 
-cd wh-token &&
-rm deployment.json ||
+cd wh-token
+rm -f deployment.json
 ntt init Mainnet
 ntt add-chain $NETWORK_1U --latest --mode burning --token $TOKEN_1 --skip-verify
 ntt add-chain $NETWORK_2U --latest --mode burning --token $TOKEN_2 --skip-verify
 cp deployment.json ../packages/nextjs/contracts/deployment.json
-cp deployment.json ../packages/hardhat/deploy/deployment.json ||
+cp deployment.json ../packages/hardhat/deploy/deployment.json
 cd ..
 yarn deploy --tags SetMinter --network $NETWORK_1
 yarn deploy --tags SetMinter --network $NETWORK_2
