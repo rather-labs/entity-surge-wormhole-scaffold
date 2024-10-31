6b0187838d91deec5b8b74c4c628754de1433343
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-10-31 10:25:52 -0300

    Remove lint run stage.

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
       },
     ],
   },
-  defaultNetwork: "hardhat",
+  defaultNetwork: "arbitrumSepolia",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
