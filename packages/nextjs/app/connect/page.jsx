ac159fa245d60e7e8c2c7ed1c3dfcbb98afda6ce
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-10-31 11:20:26 -0300

    Add dyamic chain configuration.

--- packages/nextjs/app/connect/page.jsx
+++ packages/nextjs/app/connect/page.jsx
@@ -2,7 +2,6 @@
 
 import "./page.css";
 import WormholeConnect, { nttRoutes } from "@wormhole-foundation/wormhole-connect";
-import type { WormholeConnectConfig, WormholeConnectTheme } from "@wormhole-foundation/wormhole-connect";
 import deployment from "~~/contracts/deployment.json";
 
 /* export const metadata = getMetadata({
@@ -10,82 +9,58 @@ import deployment from "~~/contracts/deployment.json";
   description: "Manage Cross-Chain Transfers with Wormhole's NTT Protocol",
 }); */
 
-const wormholeConfig: WormholeConnectConfig = {
+const wormholeConfig = {
   network: "Mainnet", // from deployment.json of the NTT deployment directory
   //networks: ['sepolia', 'ArbitrumSepolia', 'BaseSepolia', 'Avalanche'], // from https://github.com/wormhole-foundation/wormhole-connect/blob/development/wormhole-connect/src/config/testnet/chains.ts#L170
-  chains: ["Arbitrum", "Polygon"],
+  chains: Object.keys(deployment.chains),
   // tokens: ['FTTsep', 'FTTsol'],  // this will limit the available tokens that can be transferred to the other chain
   // routes: ['nttManual'], // this will limit the available routes - from https://github.com/wormhole-foundation/wormhole-connect/blob/d7a6b67b18db2c8eb4a249d19ef77d0174deffbe/wormhole-connect/src/config/types.ts#L70
   rpcs: {
     Polygon: "https://polygon.llamarpc.com",
     Arbitrum: "https://arb1.arbitrum.io/rpc",
+    Optimism: "https://mainnet.optimism.io",
   },
 
   routes: [
     ...nttRoutes({
       tokens: {
-        LNCH: [
-          {
-            chain: "Arbitrum",
-            manager: deployment.chains.Arbitrum.manager,
-            token: deployment.chains.Arbitrum.token,
-            transceiver: [
-              {
-                address: deployment.chains.Arbitrum.transceivers.wormhole.address,
-                type: "wormhole",
-              },
-            ],
-          },
-          {
-            chain: "Polygon",
-            manager: deployment.chains.Polygon.manager,
-            token: deployment.chains.Polygon.token,
-            transceiver: [
-              {
-                address: deployment.chains.Polygon.transceivers.wormhole.address,
-                type: "wormhole",
-              },
-            ],
-          },
-        ],
+        LNCH: Object.keys(deployment.chains).map(chain => ({
+          chain,
+          manager: deployment.chains[chain].manager,
+          token: deployment.chains[chain].token,
+          transceiver: [
+            {
+              address: deployment.chains[chain].transceivers.wormhole.address,
+              type: "wormhole",
+            },
+          ],
+        })),
       },
     }),
     /* other routes */
   ],
-  tokensConfig: {
-    LNCHopt: {
-      key: "LNCHopt",
-      symbol: "LNCH",
-      nativeChain: "Polygon", // will be shown as native only on this chain, otherwise as "Wormhole wrapped"
-      displayName: "LNCH (OP)", // name that is displayed in the Route
-      tokenId: {
-        chain: "Polygon",
-        address: deployment.chains.Polygon.token, // token address
+  tokensConfig: Object.fromEntries(
+    Object.keys(deployment.chains).map(chain => [
+      "LNCH-" + chain,
+      {
+        key: "LNCH-" + chain,
+        symbol: "LNCH",
+        nativeChain: chain,
+        displayName: `LNCH (${chain})`,
+        tokenId: {
+          chain,
+          address: deployment.chains[chain].token,
+        },
+        coinGeckoId: "test",
+        icon: "https://wormhole.com/token.png",
+        color: "#00C3D9",
+        decimals: 18,
       },
-      coinGeckoId: "test",
-      icon: "https://wormhole.com/token.png",
-      color: "#00C3D9",
-      decimals: 18,
-    },
-
-    LNCHarb: {
-      key: "LNCHarb",
-      symbol: "LNCH",
-      nativeChain: "Arbitrum", // will be shown as native only on this chain, otherwise as "Wormhole wrapped"
-      displayName: "LNCH (ARB)", // name that is displayed in the Route
-      tokenId: {
-        chain: "Arbitrum",
-        address: deployment.chains.Arbitrum.token, // token address
-      },
-      coinGeckoId: "test",
-      icon: "https://wormhole.com/token.png",
-      color: "#00C3D9",
-      decimals: 18,
-    },
-  },
+    ]),
+  ),
 };
 
-const theme: WormholeConnectTheme = {
+const theme = {
   mode: "dark",
   primary: "#077f75",
   secondary: "#044c46",
