49659ecdaf91b03121c51fa4e06d50f055a6c10a
Martin <arandaschimpf@gmail.com>
2024-11-04 12:36:43 -0300

    Fixed typing on connect config

--- packages/nextjs/app/connect/page.tsx
+++ packages/nextjs/app/connect/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import "./page.css";
-import dynamic from "next/dynamic";
 import React, { Suspense } from "react";
+import dynamic from "next/dynamic";
+import "./page.css";
+import WormholeConnect, { WormholeConnectConfig, nttRoutes } from "@wormhole-foundation/wormhole-connect";
 
-// Dynamically import WormholeConnect
-const WormholeConnect = dynamic(
-  () => import("@wormhole-foundation/wormhole-connect").then((mod) => mod.default),
-  { ssr: false }
-);
+// Disable SSR for WormholeConnect
+const WormholeConnectNoSSR = dynamic(() => Promise.resolve(WormholeConnect), { ssr: false });
 
 // Synchronous wormholeConfig setup
-const wormholeConfig = {
+const wormholeConfig: WormholeConnectConfig = {
   network: "Testnet",
   chains: ["Sepolia", "ArbitrumSepolia"],
   rpcs: {
@@ -19,36 +17,34 @@ const wormholeConfig = {
     BaseSepolia: "https://base-sepolia-rpc.publicnode.com",
     ArbitrumSepolia: "https://sepolia-rollup.arbitrum.io/rpc",
   },
-  routes: [
-    {
-      tokens: {
-        NTTTT: [
-          {
-            chain: "Sepolia",
-            manager: "0xE2A2bedF4E404A1CCE742AcC08abBedf2c51C4dF",
-            token: "0xA0e4C6FA5dFc8d68C06C4Caefa0439B4dFC2F697",
-            transceiver: [
-              {
-                address: "0xccd630AD8030694eDe09d90991edff96ADcdBB61",
-                type: "wormhole",
-              },
-            ],
-          },
-          {
-            chain: "ArbitrumSepolia",
-            manager: "0xFb9d0D37aEe1706A8c8E722e8Bc843fb48978969",
-            token: "0x183Ae4A056566803a10d7FDF85b723FcCd54b926",
-            transceiver: [
-              {
-                address: "0x1a46812208707D009D1E871453Fb62d76d118A0f",
-                type: "wormhole",
-              },
-            ],
-          },
-        ],
-      },
+  routes: nttRoutes({
+    tokens: {
+      NTTTT: [
+        {
+          chain: "Sepolia",
+          manager: "0xE2A2bedF4E404A1CCE742AcC08abBedf2c51C4dF",
+          token: "0xA0e4C6FA5dFc8d68C06C4Caefa0439B4dFC2F697",
+          transceiver: [
+            {
+              address: "0xccd630AD8030694eDe09d90991edff96ADcdBB61",
+              type: "wormhole",
+            },
+          ],
+        },
+        {
+          chain: "ArbitrumSepolia",
+          manager: "0xFb9d0D37aEe1706A8c8E722e8Bc843fb48978969",
+          token: "0x183Ae4A056566803a10d7FDF85b723FcCd54b926",
+          transceiver: [
+            {
+              address: "0x1a46812208707D009D1E871453Fb62d76d118A0f",
+              type: "wormhole",
+            },
+          ],
+        },
+      ],
     },
-  ],
+  }),
   tokensConfig: {
     NTTTTsep: {
       key: "NTTTTsep",
@@ -102,7 +98,7 @@ const Connect = () => {
       </div>
       <Suspense fallback={<div>Loading Wormhole Connect...</div>}>
         <div className="wormhole-wrapper">
-          <WormholeConnect config={wormholeConfig} theme={theme} />
+          <WormholeConnectNoSSR config={wormholeConfig} theme={theme} />
         </div>
       </Suspense>
     </>
